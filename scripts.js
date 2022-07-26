354d0f85bd4c61f4de6888c27a86d3d86db9acb3
SofKapa <sofiakmachado@gmail.com>
2022-07-26 07:59:03 +0100

    Hamburger Menu

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
 
+    // If you click on element with 'hamburger-button' class (in this case 'MENU')... 
+
+    $('.hamburger-button').click(function(){
+  
+        // ...the element with the class of 'mobile-menu' (in this case our div containing the links) will fade toggle:
+        $('.mobile-menu').slideToggle(); 
+    
+        // ...the hamburger element itself:
+        $(this).toggleClass('active'); 
+
+    });
+
+});
 
 
