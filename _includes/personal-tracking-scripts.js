0fb4e04959f2bc53b78c5f0cd5a5a03944fd83fa
SofKapa <sofiakmachado@gmail.com>
2024-07-03 19:07:47 +1000

    Cookies - Testing

--- _includes/personal-tracking-scripts.js
+++
@@ -1,7 +0,0 @@
-// Google Analytics - (second script without the tags)
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-9M78DLKD8V');
