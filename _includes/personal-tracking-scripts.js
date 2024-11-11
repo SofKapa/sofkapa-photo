7302c238487d35959fcd1361be9c5a1d301b1635
SofKapa <sofiakmachado@gmail.com>
2024-11-11 13:52:22 +1100

    Activating Slick gallery

--- _includes/personal-tracking-scripts.js
+++ _includes/personal-tracking-scripts.js
@@ -3,3 +3,9 @@
     gtag('js', new Date());
 
     gtag('config', 'G-9M78DLKD8V');
+
+
+    // To activate our Slick sliding gallery/testimonials 
+    $('.slider-container').slick({
+    // Setting name: setting-value
+    });
