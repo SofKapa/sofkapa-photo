a618707bbcfd9b339b4e23b89f23fe0be45b592e
SofKapa <sofiakmachado@gmail.com>
2024-11-21 12:42:34 +1100

    Slick.js - Slider settings

--- scripts.js
+++ scripts.js
@@ -32,13 +32,13 @@ $( document ).ready(function() {
     $('.slider-container').slick({
         autoplay: true, // Do we want it to autoplay? true or false
         autoplaySpeed: 2000, // How long between each slide when auto-playing
-        speed: 500, // How fast is the transition in milliseconds
+        speed: 1500, // How fast is the transition in milliseconds
         arrows: false, // Do you want to show arrows to trigger each slide
-        accessibility: true, // Enables keyboard tabbing and arrow key navigation
+        accessibility: false, // Enables keyboard tabbing and arrow key navigation
         dots: true, // Enables the dots below to show how many slides
-        fade: false, // Changes the animate from slide to fade if true
-        infinite: false, // When true, means that it will scroll in a circle
-        pauseOnHover: false, // When true means the autoplay pauses when hovering
+        fade: true, // Changes the animate from slide to fade if true
+        infinite: true, // When true, means that it will scroll in a circle
+        pauseOnHover: true, // When true means the autoplay pauses when hovering
         pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
     });
 
